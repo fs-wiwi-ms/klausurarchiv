69d0e9ce2feb96a9205ce9eae0b698e755f8593a
Tobias Hoge <tbho@tbho.de>
2019-06-24 14:51:49 +0200

    change prod conf

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,10 @@ use Mix.Config
 config :klausurarchiv, KlausurarchivWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:clappline, :vsn),
 
 # Do not print debug messages in production
 config :logger, level: :info
