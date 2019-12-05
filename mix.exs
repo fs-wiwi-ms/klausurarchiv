2a88287ce5878adf1d5bbd0588cd440f498e0700
Tobias Hoge <tbho@tbho.de>
2019-12-05 15:57:39 +0100

    change mix.exs

--- mix.exs
+++ mix.exs
@@ -37,7 +37,6 @@ defmodule Klausurarchiv.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_slime, "~> 0.10.0"},
       {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.1"},
       {:plug_cowboy, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
       {:distillery, "~> 2.0"},
