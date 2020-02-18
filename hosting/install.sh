2bfc02056f05c941cefe37645cf5389da69d2bf9
Tobias Hoge <tbho@tbho.de>
2020-02-18 18:47:15 +0100

    add maintenance page

--- hosting/install.sh
+++ hosting/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ssh $1 "mkdir -p /root/klausurarchiv"
+ssh $1 "mkdir -p /root/klausurarchiv /var/www/klausurarchiv"
 scp app.env $1:/root/klausurarchiv/app.env
 scp db.env $1:/root/klausurarchiv/db.env
 scp docker-compose.yml $1:/root/klausurarchiv/docker-compose.yml
 
 scp -r setup/ $1: && ssh $1 "cd ~/setup; chmod +x setup.sh; ./setup.sh"
+scp -r nginx/klausurarchiv/ $1:/var/www/
 scp nginx/nginx.conf $1:/etc/nginx/sites-available/default && ssh $1 "sudo service nginx restart"
