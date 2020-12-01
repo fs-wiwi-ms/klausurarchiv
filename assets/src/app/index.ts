488a6888852d1f15aa96bbaab5513c6d4901a677
Tobias Hoge <tbho@tbho.de>
2020-12-01 08:23:51 +0100

    winter wonderland

--- assets/src/app/index.ts
+++ assets/src/app/index.ts
@@ -1,3 +1,17 @@
 import "@fortawesome/fontawesome-free/js/all.js";
 
+import "./snow.js";
+
+snowStorm.animationInterval = 50;
+snowStorm.followMouse = true;
+snowStorm.snowStick = false;
+snowStorm.flakesMax = 200;
+snowStorm.snowColor = '#eee';
+snowStorm.freezeOnBlur = true;
+snowStorm.snowStick = true;
+snowStorm.vMaxX = 2;
+snowStorm.vMaxY = 2;
+
+snowStorm.toggleSnow();
+
 import "./theme.sass";
