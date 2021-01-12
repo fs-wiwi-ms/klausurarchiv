2238179def1db78f17d19edd80fbadc865875b63
Tobias Hoge <tbho@tbho.de>
2021-01-12 17:12:44 +0100

    fix email

--- lib/klausurarchiv/mailer.ex
+++ lib/klausurarchiv/mailer.ex
@@ -1,24 +1,4 @@
 defmodule Klausurarchiv.Mailer do
-  use Bamboo.Mailer, otp_app: :klausurarchiv
-
-  def deliver_email(email) do
-    email
-    |> deliver_now(config: get_environment_config())
-  end
-
-  def get_environment_config() do
-    case Mix.env() do
-      :dev ->
-        %{
-          server: System.get_env("SMTP_SERVER"),
-          hostname: System.get_env("HOST"),
-          port: System.get_env("SMTP_PORT"),
-          username: System.get_env("SMTP_USERNAME"),
-          password: System.get_env("SMTP_PASSWORD")
-        }
-
-      _ ->
-        %{}
-    end
-  end
+  use Bamboo.Mailer,
+    otp_app: :klausurarchiv
 end
