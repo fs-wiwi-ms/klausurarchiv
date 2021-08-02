9c1e35343e2ccef0ea8162b1a6035f41497594b2
Tobias Hoge <tbho@tbho.de>
2021-08-02 14:28:39 +0200

    add downtime notice

--- mix.exs
+++ mix.exs
@@ -75,8 +75,6 @@ defmodule Klausurarchiv.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
       ci: ["deps.get", "test"]
     ]
